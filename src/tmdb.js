7627e8e6c4439e7e0986a4d7de7d8f3c215d6e5d
fabiotorresdev <fabiodev2022@gmail.com>
2022-12-22 23:35:02 -0300

    algumas alteraçoes

--- src/tmdb.js
+++ src/tmdb.js
@@ -30,32 +30,32 @@ export default {
                 slug: 'trending',
                 title: 'Recomendados para Você',
                 items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`),
-            }
+            };
             {
                 slug: 'topreted',
                 title: 'Em Alta',
                 items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
-            }
+            };
             {
                 slug: 'action',
                 title: 'Ação',
                 items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
-            }
+            };
             {
                 slug: 'comedy',
                 title: 'Comédia',
                 items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
-            }
+            };
             {
                 slug: 'horror',
                 title: 'Terror',
                 items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
-            }
+            };
             {
                 slug: 'romance',
                 title: 'Romance',
                 items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
-            }
+            };
             {
                 slug: 'documentary',
                 title: 'Documentario',
